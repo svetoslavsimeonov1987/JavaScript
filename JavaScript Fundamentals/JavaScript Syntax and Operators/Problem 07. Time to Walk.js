0ab2baaed63f4b1584fddc580890a0c1072afb49
svetoslavsimeonov1987 <svetoslav.simeonov@outlook.com>
2019-01-27 13:37:36 +0200

    update solution

--- JavaScript Fundamentals/JavaScript Syntax and Operators/Problem 07. Time to Walk.js
+++ JavaScript Fundamentals/JavaScript Syntax and Operators/Problem 07. Time to Walk.js
@@ -4,13 +4,13 @@ function solve(numberOfSteps, studentFootprint, studentSpeedInKM){
 
    let speedInSeconds = studentSpeedInKM / 3.6; // To convert km/h to m/s you should divide 5 to 3,6. 1 m/s = 3,6 km/h
 
-   let timeSeconds = Math.round(distanceInMeters / speedInSeconds);
+   let timeInSeconds = Math.round(distanceInMeters / speedInSeconds);
 
    let rest = Math.floor(distanceInMeters / 500);
-   timeSeconds += rest * 60;
-   let seconds = timeSeconds % 60;
-   let minutes = ((timeSeconds - seconds) / 60) % 60;
-   let hours = Math.floor((timeSeconds - seconds) / 3600);
+   timeInSeconds += rest * 60;
+   let seconds = timeInSeconds % 60;
+   let minutes = ((timeInSeconds - seconds) / 60) % 60;
+   let hours = Math.floor((timeInSeconds - seconds) / 3600);
    
    
 
